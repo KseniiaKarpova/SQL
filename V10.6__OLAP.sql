5f062ce9556b81833d6c369c95a815895efe21a1
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-04-26 18:08:08 +0300

    Update V10.6__OLAP.sql

--- V10.6__OLAP.sql
+++ V10.6__OLAP.sql
@@ -8,13 +8,13 @@ $$
  DECLARE
   m matrix;
 BEGIN
- FOR i IN 1..5 LOOP
-  FOR i IN 1..5 LOOP
-    m.points[i][j] := $1.points[i][j] +$2.points[i][j]
-  END LOOP;
- END LOOP;
-RETURN ROW(m)::matrix;
-END;
+   FOR i IN 1..5 LOOP
+      FOR j IN 1..5 LOOP
+        m.points[i][j] := $1.points[i][j] +$2.points[i][j]
+      END LOOP;
+   END LOOP;
+  RETURN ROW(m)::matrix;
+ END;
 $$ LANGUAGE plpgsql; 
 
 
