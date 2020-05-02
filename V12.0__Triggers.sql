2798b200ed51473233d6f2932a56088e0cd918fa
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-02 19:52:10 +0300

    Update V12.0__Triggers.sql

--- V12.0__Triggers.sql
+++ V12.0__Triggers.sql
@@ -52,8 +52,8 @@ $$
       format('ALTER TABLE %s ADD CONSTRAINT partition_check check ( id >= %s and id <= %s );',
       table_name_str, NEW.id / 100000 * 100000, (NEW.id / 100000 + 1) * 100000); 
     EXECUTE  
-      format('CREATE TRIGGER %s INSTEAD OF UPDATE ON %s FOR EACH ROW EXECUTE FUNCTION redirect_update();',
-      'trigger_redirect_update_' || table_name_str, table_name_str); 
+      format('CREATE TRIGGER %s INSTEAD OF UPDATE ON %s FOR EACH ROW
+       EXECUTE FUNCTION redirect_update();','trigger_redirect_update_' || table_name_str, table_name_str); 
    END IF; 
  EXECUTE 
  format('INSERT INTO %s VALUES(%s, %L);',
