49b7d49828b3bc4575dd675b6513b9174dfa5568
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-03 14:03:37 +0300

    Update V12.0__Indexes2.sql

--- V12.0__Indexes2.sql
+++ V12.0__Indexes2.sql
@@ -1,26 +1,26 @@
-CREATE TABLE public.GIN_table(
+CREATE TABLE public.GIN_tbl(
     id   serial PRIMARY KEY,
     content varchar);
 
-CREATE TABLE public.BTree_t(
+CREATE TABLE public.BTree_tbl(
     id   serial PRIMARY KEY,
     content varchar);
 
 
-CREATE TABLE public.GIST_table(
+CREATE TABLE public.GIST_tbl(
     id   serial PRIMARY KEY,
     content varchar);
 
-CREATE INDEX gin_index ON public.GIN_table USING GIN(content);
-CREATE INDEX btree_idx ON public.BTree_t USING btree(content);
-CREATE INDEX gist_index ON public.GIST_table USING GIST(content);
+CREATE INDEX gin_idx ON public.GIN_tbl USING GIN(content);
+CREATE INDEX btree_idx ON public.BTree_tbl USING btree(content);
+CREATE INDEX gist_idx ON public.GIST_tbl USING GIST(content);
 
 
-INSERT INTO public.GIN_table(content)
+INSERT INTO public.GIN_tbl(content)
 SELECT random_string() FROM generate_series(1,100000) AS g(i);
 
-INSERT INTO public.BTree_t(content)
+INSERT INTO public.BTree_tbl(content)
 SELECT random_string() FROM generate_series(1,100000) AS g(i);
 
-INSERT INTO public.GIST_table(content)
+INSERT INTO public.GIST_tbl(content)
 SELECT random_string() FROM generate_series(1,100000) AS g(i);
