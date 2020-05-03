33a4dc1c7b256d673e483519ac9e94622526383a
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-03 21:01:05 +0300

    Update insert_t2.sql

--- trg/insert_t2.sql
+++ trg/insert_t2.sql
@@ -2,9 +2,12 @@
 BEGIN;
 PREPARE ps(INTEGER, VARCHAR) AS
   INSERT INTO public.tbl_2 VALUES($1, $2);
+  DO $
+  BEGIN
   FOR i in 1..100 LOOP
     EXECUTE ps(i, 'string');
   END LOOP;
+  END$;
 DEADLLOCATE ps;
 COMMIT;
 
