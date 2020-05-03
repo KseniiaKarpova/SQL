7626358259ad37d71be7dbe3aacce360fe69d3d7
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-03 14:01:43 +0300

    Update V12.0__Indexes2.sql

--- V12.0__Indexes2.sql
+++ V12.0__Indexes2.sql
@@ -2,7 +2,7 @@ CREATE TABLE public.GIN_table(
     id   serial PRIMARY KEY,
     content varchar);
 
-CREATE TABLE public.BTree_table(
+CREATE TABLE public.BTree_t(
     id   serial PRIMARY KEY,
     content varchar);
 
@@ -12,14 +12,14 @@ CREATE TABLE public.GIST_table(
     content varchar);
 
 CREATE INDEX gin_index ON public.GIN_table USING GIN(content);
-CREATE INDEX btree_index ON public.BTree_table USING btree(content);
+CREATE INDEX btree_idx ON public.BTree_t USING btree(content);
 CREATE INDEX gist_index ON public.GIST_table USING GIST(content);
 
 
 INSERT INTO public.GIN_table(content)
 SELECT random_string() FROM generate_series(1,100000) AS g(i);
 
-INSERT INTO public.BTree_table(content)
+INSERT INTO public.BTree_t(content)
 SELECT random_string() FROM generate_series(1,100000) AS g(i);
 
 INSERT INTO public.GIST_table(content)
