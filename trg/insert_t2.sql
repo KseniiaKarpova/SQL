6f001a367ba619718ea7e64821c34241eda72984
KseniiaKarpova <31700970+KseniiaKarpova@users.noreply.github.com>
2020-05-03 20:44:26 +0300

    Update insert_t2.sql

--- trg/insert_t2.sql
+++ trg/insert_t2.sql
@@ -1,6 +1,6 @@
 BEGIN;
 PREPARE ps(INTEGER, VARCHAR) AS
   INSERT INTO public.tbl_2 VALUES($1, $2);
-  EXECUTE ps($1, $2)using SELECT generate_series(1,100)::INTEGER, 'string');
+  EXECUTE ps($1, $2) using generate_series(1,100)::INTEGER, 'string');
   DEADLLOCATE ps;
 COMMIT;
